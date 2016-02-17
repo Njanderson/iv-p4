32d4bd4999504adba52369fee7d0ec292884c069
JacobNA <jacob.norlinandersson@gmail.com>
2016-02-17 14:18:21 +0100

    Updated to mysql connection

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,22 +1,28 @@
 "use strict";
 // Import node module
 import express from 'express';
-var pgp = require('pg-promise')();//necessary because it need options or it gets very angry
+import mysql from 'mysql';
+
+var connection = mysql.createConnection({
+  host     : 'infovis16kth.cshnbc8ndr6r.us-east-1.rds.amazonaws.com',
+  port 	   : 3306,
+  database : 'winddata',
+  user     : 'infovis16user',
+  password : 'infovis16pass'
+});
 
 const router = express.Router();
 
 // Arrow functions
 router.get('/', (req, res) => {
-	var db = pgp("postgres://postgres:123456@localhost:1234/test");
-	db.manyOrNone("SELECT * from test")
-    .then(function (data) {
-        res.send(data[0])
-    })
-    .catch(function (error) {
-        console.log("ERROR:", error);
-    });
+	connection.connect();
+	connection.query('CREATE TABLE test (foo integer, bar integer);', (err, rows, fields) => {
+			console.log(err);
 
-	// res.send({message: 'kebab!'});
+		});
+
+
+	// res.send({message: '	'});
 });
 // Exporting an object as the default import for this module
 export default router;
