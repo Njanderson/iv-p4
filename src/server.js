12adedc39abcc55a3f10e978e7a75e239ecaf43b
EC2 Default User <ec2-user@ip-172-31-50-206.ec2.internal>
2016-02-25 16:57:23 +0000

    Hooked up http server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@
 import express from 'express';
 // Importing source files
 import routes from './routes/main.routes';
+// import http
+import http from 'http';
 // consts
 const app = express();
 
@@ -16,3 +18,8 @@ const server = app.listen(3000, () => {
   // string interpolation:
   console.log(`app listening at http://${address}:${port}`);
 });
+
+http.createServer(app).listen(80, function(){
+  console.log("Express server listening on port " + 80);
+});
+
